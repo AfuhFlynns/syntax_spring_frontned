8bf03a1f703f616358810f57e42210eaf6d67c66
Afuh Flyine Tembeng <flyinnsafuh@gmail.com>
2024-12-21 15:15:55 +0100

    Update axios.config.ts

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -3,6 +3,7 @@ import { baseUrl } from "../utils/constants/constants";
 
 export default axios.create({
   baseURL: baseUrl, // Configure axios to wend cookies and more
+  withCredentials: true,
 });
 
 export const protectedAxios = axios.create({
